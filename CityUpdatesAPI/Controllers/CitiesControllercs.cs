6940cc41c590da46031bc918c9c540f64ac8d8ba
Fredricc <kfred45.fk@gmail.com>
2023-02-07 02:55:12 +0300

    Demo.Returning Resources Part 1

---
+++ CityUpdatesAPI/Controllers/CitiesControllercs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityUpdatesAPI.Controllers
+{
+    [ApiController]
+    public class CitiesControllercs: ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            new JsonResult(
+                new List<object>{
+                new { id = 1, Name = "New York City"},
+                new { id = 2, Name = "Nairobi"}
+                });
+        }
+    }
+}
