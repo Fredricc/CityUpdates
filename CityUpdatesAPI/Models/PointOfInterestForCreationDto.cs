bf4c5ccaad43bed98a346f28236cf28aab9c4b6d
Fredricc <kfred45.fk@gmail.com>
2023-02-10 22:45:37 +0300

    Demo.Validating Input

--- CityUpdatesAPI/Models/PointOfInterestForCreationDto.cs
+++ CityUpdatesAPI/Models/PointOfInterestForCreationDto.cs
@@ -1,9 +1,14 @@
-﻿namespace CityUpdatesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityUpdatesAPI.Models
 {
     public class PointOfInterestForCreationDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
