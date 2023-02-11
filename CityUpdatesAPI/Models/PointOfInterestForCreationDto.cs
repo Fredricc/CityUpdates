e3880991ed89f17b3b6bc487cdcb3ba0cea2666b
Fredricc <kfred45.fk@gmail.com>
2023-02-11 04:47:08 +0300

    Demo.Validating Input

--- CityUpdatesAPI/Models/PointOfInterestForCreationDto.cs
+++ CityUpdatesAPI/Models/PointOfInterestForCreationDto.cs
@@ -4,7 +4,7 @@ namespace CityUpdatesAPI.Models
 {
     public class PointOfInterestForCreationDto
     {
-        [Required]
+        [Required(ErrorMessage = "You should provide a name value")]
         [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
