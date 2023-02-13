53216d180fe49ea5ccc1d938da077dee9c67a7a0
Fredricc <kfred45.fk@gmail.com>
2023-02-13 14:49:47 +0300

    Demo.Registering a Service by Interface

--- CityUpdatesAPI/Controllers/PointsOfInterestController.cs
+++ CityUpdatesAPI/Controllers/PointsOfInterestController.cs
@@ -11,13 +11,16 @@ namespace CityUpdatesAPI.Controllers
     public class PointsOfInterestController : ControllerBase
     {
         private readonly ILogger<PointsOfInterestController> _logger;
-        private readonly LocalMailService _mailService;
+        private readonly IMailService _mailService;
+        private readonly CitiesDataStore _citiesDataStore;
 
         public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
-            LocalMailService MailService)
+            IMailService mailService,
+            CitiesDataStore citiesDataStore)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _mailService = MailService ?? throw new ArgumentNullException(nameof(MailService));
+            _mailService = mailService ?? throw new ArgumentNullException(nameof(mailService));
+            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
         }
 
         [HttpGet]
@@ -27,7 +30,7 @@ namespace CityUpdatesAPI.Controllers
             {
                 //throw new Exception("Exception sample.");
 
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == cityId);
 
             if ( city == null ) 
             {
@@ -50,7 +53,7 @@ namespace CityUpdatesAPI.Controllers
         public ActionResult<PointOfInterestDto> GetPointOfInterest(
             int cityId, int pointOfInterestId)
         {
-            var city = CitiesDataStore.Current.Cities
+            var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
@@ -72,7 +75,7 @@ namespace CityUpdatesAPI.Controllers
             int cityId,
             PointOfInterestForCreationDto pointOfInterest)
         { 
-            var city = CitiesDataStore.Current.Cities
+            var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             { 
@@ -80,7 +83,7 @@ namespace CityUpdatesAPI.Controllers
             }
 
             // demo purposes - to be improved
-            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+            var maxPointOfInterestId = _citiesDataStore.Cities.SelectMany(
                 c => c.PointsOfInterest).Max(p => p.Id);
 
             var finalPointOfInterest = new PointOfInterestDto()
@@ -106,7 +109,7 @@ namespace CityUpdatesAPI.Controllers
         public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId,
             PointOfInterestForUpdateDto pointOfInterest)
         { 
-            var city = CitiesDataStore.Current.Cities
+            var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if(city == null) 
             {
@@ -131,7 +134,7 @@ namespace CityUpdatesAPI.Controllers
             int cityId, int pointOfInterestId,
             JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument)
         {
-            var city = CitiesDataStore.Current.Cities
+            var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
@@ -171,7 +174,7 @@ namespace CityUpdatesAPI.Controllers
         [HttpDelete("{pointOfInterestId}")]
         public ActionResult DeletePointOfInterest(int cityId, int pointOfInterestId)
         {
-            var city = CitiesDataStore.Current.Cities
+            var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
---
+++ CityUpdatesAPI/Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace CityUpdatesAPI.Services
+{
+    public interface IMailService
+    {
+        void Send(string subject, string message);
+    }
+}
