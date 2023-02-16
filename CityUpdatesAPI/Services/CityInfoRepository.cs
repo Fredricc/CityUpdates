f73541a29eea75242c2cda3d0bacf41e7612514d
Fredricc <kfred45.fk@gmail.com>
2023-02-16 06:24:41 +0300

    Writing Asynchronous Code

---
+++ CityUpdatesAPI/Services/CityInfoRepository.cs
@@ -0,0 +1,29 @@
+﻿using CityUpdatesAPI.Entities;
+
+namespace CityUpdatesAPI.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        public Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<City?> GetCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetPointOfInterestForCityAsync(
+            int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest?> GetPointOfInterestForCityAsync(
+            int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CityUpdatesAPI/Services/ICityInfoRepository.cs
+++ CityUpdatesAPI/Services/ICityInfoRepository.cs
@@ -5,5 +5,11 @@ namespace CityUpdatesAPI.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCitiesAsync();
+
+        Task<City?> GetCityAsync( int cityId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, 
+            int pointOfInterestId);
     }
 }
