4260a17d27fbc78cb9007f5f1d97db0c90a98730
Fredricc <kfred45.fk@gmail.com>
2023-02-20 06:55:32 +0300

    Demo.Searching Through Resources

--- CityUpdatesAPI/Services/ICityInfoRepository.cs
+++ CityUpdatesAPI/Services/ICityInfoRepository.cs
@@ -5,7 +5,7 @@ namespace CityUpdatesAPI.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCitiesAsync();
-        Task<IEnumerable<City>> GetCitiesAsync(string? name);
+        Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery);
 
         Task<City?> GetCityAsync( int cityId, bool includePointsOfInterest);
 
